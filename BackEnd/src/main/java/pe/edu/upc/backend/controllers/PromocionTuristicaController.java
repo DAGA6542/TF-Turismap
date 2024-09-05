655afc2264c432d1c84a16b9f8e7b4d4f2ad97ab
Joseph <u202022089@upc.edu.pe>
2024-09-05 05:23:33 -0500

    Corregido.

--- BackEnd/src/main/java/pe/edu/upc/backend/controllers/PromocionTuristicaController.java
+++ BackEnd/src/main/java/pe/edu/upc/backend/controllers/PromocionTuristicaController.java
@@ -1,21 +1,18 @@
 package pe.edu.upc.backend.controllers;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.backend.dtos.PromocionTuristicaDTO;
 import pe.edu.upc.backend.entities.PromocionTuristica;
 import pe.edu.upc.backend.serviceinterfaces.IPromocionTuristicaService;
-
 import java.util.List;
 import java.util.stream.Collectors;
-
 @RestController
 @RequestMapping("/promocionesturisticas")
 public class PromocionTuristicaController {
     @Autowired
     private IPromocionTuristicaService pS;
-    @GetMapping
+    @PostMapping
     public void agregar(@RequestBody PromocionTuristicaDTO promocionTuristicaDTO) {
         ModelMapper m = new ModelMapper();
         PromocionTuristica promocion = m.map(promocionTuristicaDTO, PromocionTuristica.class);
