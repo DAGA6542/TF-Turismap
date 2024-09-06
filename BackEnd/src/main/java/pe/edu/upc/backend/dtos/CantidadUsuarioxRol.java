c535d376d56982471c04ec826faebccfde5d7717
RomarioCasas <dangerous12340@gmail.com>
2024-09-06 13:52:53 -0500

    Add files via upload

---
+++ BackEnd/src/main/java/pe/edu/upc/backend/dtos/CantidadUsuarioxRol.java
@@ -0,0 +1,24 @@
+package pe.edu.upc.backend.dtos;
+
+public class CantidadUsuarioxRol {
+public String NombreRol;
+public int CantidadUsuario;
+
+    public String getNombreRol() {
+        return NombreRol;
+    }
+
+    public void setNombreRol(String nombreRol) {
+        NombreRol = nombreRol;
+    }
+
+    public int getCantidadUsuario() {
+        return CantidadUsuario;
+    }
+
+    public void setCantidadUsuario(int cantidadUsuario) {
+        CantidadUsuario = cantidadUsuario;
+    }
+
+
+}
