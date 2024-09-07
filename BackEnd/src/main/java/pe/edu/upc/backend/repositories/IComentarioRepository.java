7020e8933039c9cb72fae7f30656bc3ecd465e39
Joseph <u202022089@upc.edu.pe>
2024-09-07 02:56:09 -0500

    Comentario jpql añadido.

--- BackEnd/src/main/java/pe/edu/upc/backend/repositories/IComentarioRepository.java
+++ BackEnd/src/main/java/pe/edu/upc/backend/repositories/IComentarioRepository.java
@@ -1,7 +1,21 @@
 package pe.edu.upc.backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.backend.entities.Comentario;
+import java.time.LocalDate;
+import java.util.List;
 @Repository
 public interface IComentarioRepository extends JpaRepository<Comentario, Integer> {
+    @Query("SELECT c FROM Comentario c WHERE c.idUsuario.idUsuario = :idUsuario")
+    List<Comentario> obtenerComentariosPorUsuario(@Param("idUsuario") int idUsuario); // Buscar todos los comentarios por usuario
+    @Query("SELECT c FROM Comentario c WHERE c.idNegocio.idNegocio = :idNegocio")
+    List<Comentario> buscarComentariosPorNegocio(@Param("idNegocio") int idNegocio); // Buscar todos los comentarios por negocio
+    @Query("SELECT c FROM Comentario c WHERE c.idTurismo.idTurismo = :idTurismo")
+    List<Comentario> buscarComentariosPorTurismo(@Param("idTurismo") int idTurismo); // Buscar comentarios por sitio turistico
+    @Query("SELECT c FROM Comentario c WHERE c.fechaComentario <= :fecha")
+    List<Comentario> obtenerComentariosHastaFecha(@Param("fecha") LocalDate fecha); // comentarios recientes hasta una fecha especifica
+    @Query("SELECT c FROM Comentario c WHERE LOWER(c.textoComentario) LIKE LOWER(CONCAT('%', :texto, '%'))")
+    List<Comentario> buscarComentario(@Param("texto") String texto); // Buscar comentario de tesxto
 }
--- BackEnd/src/main/java/pe/edu/upc/backend/serviceinterfaces/IComentarioService.java
+++ BackEnd/src/main/java/pe/edu/upc/backend/serviceinterfaces/IComentarioService.java
@@ -1,9 +1,18 @@
 package pe.edu.upc.backend.serviceinterfaces;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pe.edu.upc.backend.entities.Comentario;
+
+import java.time.LocalDate;
 import java.util.List;
 public interface IComentarioService {
     public void insert(Comentario comentario);
     public List<Comentario> list();
     public void update(Comentario comentario);
     public void delete(int idComentario);
+    public List<Comentario> obtenerComentariosPorUsuario(int idUsuario);
+    public List<Comentario> buscarComentariosPorNegocio(int idNegocio);
+    public List<Comentario> buscarComentariosPorTurismo(int idTurismo);
+    public List<Comentario> obtenerComentariosHastaFecha(LocalDate fecha);
+    public List<Comentario> buscarComentario(String texto);
 }
