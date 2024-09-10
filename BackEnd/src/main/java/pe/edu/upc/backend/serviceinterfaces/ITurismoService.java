8b9e08741da439529230458dd0ef1cd8249fec27
DAGA6542 <u202120374@upc.edu.pe>
2024-09-09 19:18:00 -0500

    Add files via upload

--- BackEnd/src/main/java/pe/edu/upc/backend/serviceinterfaces/ITurismoService.java
+++ BackEnd/src/main/java/pe/edu/upc/backend/serviceinterfaces/ITurismoService.java
@@ -1,4 +1,6 @@
 package pe.edu.upc.backend.serviceinterfaces;
+import org.springframework.data.repository.query.Param;
+import pe.edu.upc.backend.entities.Comentario;
 import pe.edu.upc.backend.entities.Turismo;
 import java.util.List;
 public interface ITurismoService {
@@ -6,4 +8,6 @@ public interface ITurismoService {
     public List<Turismo> list();
     public void update(Turismo turismo);
     public void delete(int idTurismo);
+    public List<Turismo> listarPorNumero(int idTurismo);
+    public List<Turismo> listarlugarturisticoporciudad1(String nombreCiudad);
 }
