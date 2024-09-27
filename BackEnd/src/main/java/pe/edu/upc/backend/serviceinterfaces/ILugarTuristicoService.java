85498022b951bd29e0717066b29c288939ecf867
DAGA6542 <u202120374@upc.edu.pe>
2024-09-27 04:17:41 -0500

    Update ILugarTuristicoService.java

--- BackEnd/src/main/java/pe/edu/upc/backend/serviceinterfaces/ILugarTuristicoService.java
+++ BackEnd/src/main/java/pe/edu/upc/backend/serviceinterfaces/ILugarTuristicoService.java
@@ -1,9 +1,24 @@
-package pe.edu.upc.backend.serviceinterfaces;
+package pe.edu.upc.backend.serviceimplements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pe.edu.upc.backend.entities.LugarTuristico;
+import pe.edu.upc.backend.repositories.ILugarTuristicoRepository;
+import pe.edu.upc.backend.serviceinterfaces.ILugarTuristicoService;
 import java.util.List;
-public interface ILugarTuristicoService {
-    public void insert(LugarTuristico lugarTuristico);
-    public List<LugarTuristico> list();
-    public void update(LugarTuristico lugarTuristico);
-    public void delete(int idLugarTuristico);
+@Service
+public class LugarTuristicoImplement implements ILugarTuristicoService {
+    @Autowired
+    private ILugarTuristicoRepository lR;
+    @Override
+    public void insert(LugarTuristico lugarTuristico) { lR.save(lugarTuristico); }
+    @Override
+    public List<LugarTuristico> list() { return lR.findAll(); }
+    @Override
+    public void update(LugarTuristico lugarTuristico) { lR.save(lugarTuristico); }
+    @Override
+    public void delete(int idLugarTuristico) { lR.deleteById(idLugarTuristico); }
+    @Override
+    public List<LugarTuristico> listarPorNumero(String nombreLugarTuristico){return lR.listarPorNumero(nombreLugarTuristico);};
+    @Override
+    public List<LugarTuristico> listarlugarturisticoporciudad(String nombreCiudad){return lR.listarlugarturisticoporciudad(nombreCiudad);};
 }
