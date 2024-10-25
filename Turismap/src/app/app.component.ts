077e6d1392026067aa04e4bcbad5ccca5a177302
Joseph <u202022089@upc.edu.pe>
2024-10-25 18:57:07 -0500

    Paises agregados.

--- Turismap/src/app/app.component.ts
+++ Turismap/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { PromocionComponent } from './components/promocion/promocion.component';
+import { PaisComponent } from './components/pais/pais.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UsuarioComponent, PromocionComponent, PaisComponent, MatToolbarModule, RouterLink,
+    MatMenuModule, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Turismap/src/app/app.routes.ts
+++ Turismap/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { PaisComponent } from './components/pais/pais.component';
+import { PromocionComponent } from './components/promocion/promocion.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { InsertarPromocionComponent } from './components/promocion/insertar-promocion/insertar-promocion.component';
+import { ListarPromocionComponent } from './components/promocion/listar-promocion/listar-promocion.component';
+import { InsertarUsuarioComponent } from './components/usuario/insertar-usuario/insertar-usuario.component';
+import { ListarUsuarioComponent } from './components/usuario/listar-usuario/listar-usuario.component';
+import { InsertarPaisComponent } from './components/pais/insertar-pais/insertar-pais.component';
+import { ListarPaisComponent } from './components/pais/listar-pais/listar-pais.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'pais', component: PaisComponent,
+        children: [
+            { path: 'insertar', component: InsertarPaisComponent },
+            { path: 'listar', component: ListarPaisComponent }
+        ],
+        
+    },
+    {
+        path: 'promocion', component: PromocionComponent,
+        children: [
+            { path: 'insertar', component: InsertarPromocionComponent },
+            { path: 'listar', component: ListarPromocionComponent }
+        ],
+    },
+    {
+        path: 'usuario', component: UsuarioComponent,
+        children: [
+            { path: 'insertar', component: InsertarUsuarioComponent },
+            { path: 'listar', component: ListarUsuarioComponent }
+        ],
+    },
+];
