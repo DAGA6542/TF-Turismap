db655da7cab5d2d9326a7d82b119fbcb75bdecd4
KEVIN SANTAMARIA <ka3445793@gmail.com>
2024-11-03 14:06:48 -0500

    Lista de Lugares Turísticos

--- Turismap/src/app/components/lugarturistico/listar-lugarturistico/listar-lugarturistico.component.ts
+++ Turismap/src/app/components/lugarturistico/listar-lugarturistico/listar-lugarturistico.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { RouterLink } from '@angular/router';
+import { LugarTuristico } from '../../../models/lugarturistico';
+import { LugarturisticoService } from '../../../services/lugarturistico.service';
 
 @Component({
   selector: 'app-listar-lugarturistico',
   standalone: true,
-  imports: [],
+  imports: [
+    MatPaginatorModule, MatTableModule,
+    MatIconModule, RouterLink, MatTableModule,
+    MatButtonModule, MatDialogModule, CommonModule,
+  ],
   templateUrl: './listar-lugarturistico.component.html',
   styleUrl: './listar-lugarturistico.component.css'
 })
 export class ListarLugarturisticoComponent {
+  displayedColumns: string[] = [
+    'idLugarTuristico',
+    'nombreLugarTuristico',
+    'descripcionLugarTuristico',
+    'numeroLugarTuristico',
+    'nombreCiudad',
+    'accion1',
+    'accion2'];
 
+  datasource: MatTableDataSource<LugarTuristico> = new MatTableDataSource();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private ltS: LugarturisticoService, private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.ltS.list().subscribe((data) => {
+      this.datasource = new MatTableDataSource(data);
+      this.datasource.paginator = this.paginator;
+    });
+    this.ltS.getList().subscribe((data) => {
+      this.datasource = new MatTableDataSource(data);
+    });
+  }
+  openDialog(id: number): void { }
+  delete(id: number) {
+    this.ltS.delete(id).subscribe((data) => {
+      this.ltS.list().subscribe((data) => {
+        this.ltS.setList(data);
+      });
+    });
+  }
 }
