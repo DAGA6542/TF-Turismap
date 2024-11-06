e1e0bf6969fb636c630cf6139b6ffdf8a259fa7e
DAGA6542 <u202120374@upc.edu.pe>
2024-11-05 23:19:07 -0500

    Avance

--- Turismap/src/app/components/lugarturistico/insertar-lugarturistico/insertar-lugarturistico.component.ts
+++ Turismap/src/app/components/lugarturistico/insertar-lugarturistico/insertar-lugarturistico.component.ts
@@ -1,12 +1,88 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
+import { LugarTuristico } from '../../../models/lugarturistico';
+import { LugarturisticoService } from '../../../services/lugarturistico.service';
 
 @Component({
   selector: 'app-insertar-lugarturistico',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule,CommonModule,NgIf, MatButtonModule,MatInputModule,ReactiveFormsModule,
+    RouterLink, MatSelectModule, FormsModule],
   templateUrl: './insertar-lugarturistico.component.html',
   styleUrl: './insertar-lugarturistico.component.css'
 })
 export class InsertarLugarturisticoComponent {
+  form:FormGroup = new FormGroup({});
+  lugarturistico: LugarTuristico = new LugarTuristico()
+  edicion: boolean = false;
+  id:number = 0;
+  //
+  constructor(
+    private LT:LugarturisticoService,
+    private router:Router,
+    private formbuilder: FormBuilder,
+    private route: ActivatedRoute 
+  ) {}
+  ngOnInit(): void {
+    this.route.params.subscribe((data:Params)=> {
+      this.id = data['id'];
+      this.edicion = data['id']  > 0;
+      this.init()
+    });
 
+    this.form = this.formbuilder.group({
+      hidLugarTuristico: [''],
+      hnombreLugarTuristico: ['', Validators.required],
+      hdescripcionLugarTuristico: ['', Validators.required],
+      hnumeroLugarTuristico: ['', Validators.required],
+      hidCiudad: ['', Validators.required],
+      
+    });
+  }
+
+  insertar(): void {
+    if (this.form.valid) {
+      this.lugarturistico.idLugarTuristico = this.form.value.hidLugarTuristico;
+      this.lugarturistico.nombreLugarTuristico= this.form.value.hnombreLugarTuristico;
+      this.lugarturistico.descripcionLugarTuristico=this.form.value.hdescripcionLugarTuristico;
+      this.lugarturistico.numeroLugarTuristico=this.form.value.hnumeroLugarTuristico;
+      this.lugarturistico.idCiudad=this.form.value.hidCiudad;
+      
+
+    
+      if (this.edicion) {
+        this.LT.update(this.lugarturistico).subscribe((data)=>{
+          this.LT.list().subscribe((data)=>{
+            this.LT.setList(data);
+          });
+        });
+      } else {
+        this.LT.insert(this.lugarturistico).subscribe((data) => {
+          this.LT.list().subscribe((data) => {
+            this.LT.setList(data);
+          });
+        });
+      }
+      this.router.navigate(['lugarturistico']);
+    }
+  }
+  init() {
+    if (this.edicion) {
+      this.LT.listId(this.id).subscribe((data) => {
+        this.form = new FormGroup({
+          hidLugarTuristico: new FormControl(data.idLugarTuristico),
+          hnombreLugarTuristico: new FormControl(data.nombreLugarTuristico),
+          hdescripcionLugarTuristico: new FormControl(data.descripcionLugarTuristico),
+          hnumeroLugarTuristico: new FormControl(data.numeroLugarTuristico),
+          hidCiudad: new FormControl(data.idCiudad),
+        });
+      });
+    }
+  }
 }
--- Turismap/src/app/components/promocion/listar-promocion/listar-promocion.component.ts
+++ Turismap/src/app/components/promocion/listar-promocion/listar-promocion.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Promocion } from '../../../models/promocion';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { PromocionService } from '../../../services/promocion.service';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-listar-promocion',
   standalone: true,
-  imports: [],
+  imports: [MatPaginatorModule, MatTableModule,
+    MatIconModule, RouterLink, MatTableModule,
+    MatButtonModule, MatDialogModule],
   templateUrl: './listar-promocion.component.html',
   styleUrl: './listar-promocion.component.css'
 })
-export class ListarPromocionComponent {
+export class ListarPromocionComponent implements OnInit{
+  displayedColumns: string[] = [    
+    'idPromocion','nombrePromocion','descripcionPromocion','precioPromocion',
+    'descuentoPromocion','fechaInicioPromocion','fechaFinPromocion'];
 
+    datasource: MatTableDataSource<Promocion> = new MatTableDataSource();
+
+    @ViewChild(MatPaginator) paginator! : MatPaginator;
+
+    constructor(private promS: PromocionService, private dialog:MatDialog){}
+
+    ngOnInit(): void{
+      this.promS.list().subscribe((data)=>{
+        this.datasource = new MatTableDataSource(data);
+      });
+      this.promS.getList().subscribe((data)=>{
+        this.datasource = new MatTableDataSource(data);
+      });
+    }
+    openDialog(id: number): void{}
+    delete(id: number){
+      this.promS.delete(id).subscribe((data)=>{
+        this.promS.list().subscribe((data)=>{
+          this.promS.setList(data);
+        });
+      });
+    }
 }
