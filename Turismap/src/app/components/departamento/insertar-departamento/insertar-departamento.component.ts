fd5ceebe4934eecb380f66e199d95380ac863726
DAGA6542 <u202120374@upc.edu.pe>
2024-11-08 02:46:50 -0500

    Update insertar-departamento.component.ts

--- Turismap/src/app/components/departamento/insertar-departamento/insertar-departamento.component.ts
+++ Turismap/src/app/components/departamento/insertar-departamento/insertar-departamento.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,82 +8,60 @@ import { MatSelectModule } from '@angular/material/select';
 import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
 import { Departamento } from '../../../models/departamento';
 import { DepartamentoService } from '../../../services/departamento.service';
+import { Pais } from '../../../models/pais';
+import { PaisService } from '../../../services/pais.service';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-insertar-departamento',
   standalone: true,
+  providers: [provideNativeDateAdapter()],
   imports: [MatFormFieldModule, CommonModule, NgIf, MatButtonModule, MatInputModule, ReactiveFormsModule,
-    RouterLink, MatSelectModule, FormsModule],
+    RouterLink, MatSelectModule, FormsModule, MatDatepickerModule],
+
   templateUrl: './insertar-departamento.component.html',
   styleUrl: './insertar-departamento.component.css'
 })
-export class InsertarDepartamentoComponent {
+export class InsertarDepartamentoComponent implements OnInit {
   form: FormGroup = new FormGroup({});
+  listaPaises: Pais[] = [];
   departamento: Departamento = new Departamento()
   edicion: boolean = false;
   id: number = 0;
   //
   constructor(
+    private formbuilder: FormBuilder,
+    private pS: PaisService,
     private Dep: DepartamentoService,
     private router: Router,
-    private formbuilder: FormBuilder,
-    private route: ActivatedRoute
   ) { }
   ngOnInit(): void {
-    this.route.params.subscribe((data: Params) => {
-      this.id = data['id'];
-      this.edicion = data['id'] > 0;
-      this.init()
-    });
     this.form = this.formbuilder.group({
-      idDepartamento: [''],
-      nombreDepartamento: ['', Validators.required],
-      capitalDepartamento: ['', Validators.required],
-      poblacionDepartamento: ['', Validators.required],
-      superficieDepartamento: ['', Validators.required],
-      idPais: ['', Validators.required],
-
+      hnombreDepartamento: ['', Validators.required],
+      hcapitalDepartamento: ['', Validators.required],
+      hpoblacionDepartamento: ['', Validators.required],
+      hsuperficieDepartamento: ['', Validators.required],
+      hPais: ['', Validators.required],
+    });
+    this.pS.list().subscribe((data) => {
+      this.listaPaises = data;
     });
   }
   insertar(): void {
     if (this.form.valid) {
-      this.departamento.idDepartamento = this.form.value.hidDepartamento;
       this.departamento.nombreDepartamento = this.form.value.hnombreDepartamento;
       this.departamento.capitalDepartamento = this.form.value.hcapitalDepartamento;
       this.departamento.poblacionDepartamento = this.form.value.hpoblacionDepartamento;
       this.departamento.superficieDepartamento = this.form.value.hsuperficieDepartamento;
-      this.departamento.idPais = this.form.value.hidPais;
-
-      if (this.edicion) {
-        this.Dep.update(this.departamento).subscribe((data) => {
-          this.Dep.list().subscribe((data) => {
-            this.Dep.setList(data);
-          });
-        });
-      } else {
-        this.Dep.insert(this.departamento).subscribe((data) => {
-          this.Dep.list().subscribe((data) => {
-            this.Dep.setList(data);
-          });
-        });
-      }
-      this.router.navigate(['lugarturistico']);
-    }
-  }
-  init() {
-    if (this.edicion) {
-      this.Dep.listId(this.id).subscribe((data) => {
-        this.form = new FormGroup({
-          hidDepartamento: new FormControl(data.idDepartamento),
-          hnombreDepartamento: new FormControl(data.nombreDepartamento),
-          hcapitalDepartamento: new FormControl(data.capitalDepartamento),
-          hpoblacionDepartamento: new FormControl(data.poblacionDepartamento),
-          hsuperficieDepartamento: new FormControl(data.superficieDepartamento),
-          hidPais: new FormControl(data.idPais),
-          
+      this.departamento.idPais.idPais = this.form.value.hPais;
+      
+      this.Dep.insert(this.departamento).subscribe((data) => {
+        this.Dep.list().subscribe((data) => {
+          this.Dep.setList(data);
         });
       });
+      this.router.navigate(['departamento']);
     }
   }
-
 }
