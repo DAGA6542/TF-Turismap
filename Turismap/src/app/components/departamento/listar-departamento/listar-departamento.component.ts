5953962f7bc8b219b843deec5417a315031adc77
DAGA6542 <u202120374@upc.edu.pe>
2024-11-08 02:47:43 -0500

    Update listar-departamento.component.ts

--- Turismap/src/app/components/departamento/listar-departamento/listar-departamento.component.ts
+++ Turismap/src/app/components/departamento/listar-departamento/listar-departamento.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Departamento } from '../../../models/departamento';
+import { DepartamentoService } from '../../../services/departamento.service';
 
 @Component({
   selector: 'app-listar-departamento',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './listar-departamento.component.html',
   styleUrl: './listar-departamento.component.css'
 })
 export class ListarDepartamentoComponent {
-
+  datasource: MatTableDataSource<Departamento> = new MatTableDataSource();
+  displayedColumns: string[] = [
+    'ctt1',
+    'ctt2',
+    'ctt3',
+    'ctt4',
+    'ctt5',
+    'ctt6',
+  ]
+    constructor(private DepS: DepartamentoService) {}
+    ngOnInit(): void {
+      this.DepS.list().subscribe((data) => {
+        this.datasource = new MatTableDataSource(data);
+      });
+      this.DepS.getList().subscribe((data) => {
+        this.datasource = new MatTableDataSource(data);
+      });
+    }
 }
