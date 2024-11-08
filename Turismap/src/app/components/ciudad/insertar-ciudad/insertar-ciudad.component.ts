6d063eafeec027d9cf14ca02f0f8df641bf09906
DAGA6542 <u202120374@upc.edu.pe>
2024-11-08 02:49:20 -0500

    Update insertar-ciudad.component.ts

--- Turismap/src/app/components/ciudad/insertar-ciudad/insertar-ciudad.component.ts
+++ Turismap/src/app/components/ciudad/insertar-ciudad/insertar-ciudad.component.ts
@@ -8,89 +8,65 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { Ciudad } from '../../../models/ciudad';
 import { CiudadService } from '../../../services/ciudad.service';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { Departamento } from '../../../models/departamento';
+import { DepartamentoService } from '../../../services/departamento.service';
 
 @Component({
   selector: 'app-insertar-ciudad',
   standalone: true,
-  imports: [MatFormFieldModule,CommonModule,NgIf, MatButtonModule,MatInputModule,ReactiveFormsModule,
-    RouterLink, MatSelectModule, FormsModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatFormFieldModule, CommonModule, NgIf, MatButtonModule, MatInputModule, ReactiveFormsModule,
+    RouterLink, MatSelectModule, FormsModule, MatDatepickerModule],
   templateUrl: './insertar-ciudad.component.html',
   styleUrl: './insertar-ciudad.component.css'
 })
-export class InsertarCiudadComponent {
+export class InsertarCiudadComponent implements OnInit{
   form:FormGroup = new FormGroup({});
+  listarDepartamento: Departamento[]=[];
   ciudad: Ciudad = new Ciudad()
   edicion: boolean = false;
   id:number = 0;
-
-  mensaje: string = '';
+  //
 
   constructor(
-    private cS: CiudadService,
-    private router:Router,
     private formbuilder: FormBuilder,
-    private route: ActivatedRoute
+    private Dep: DepartamentoService,
+    private ciuS: CiudadService,
+    private router:Router,
   ) {}
-  ngOnInit(): void {
-    this.route.params.subscribe((data:Params)=> {
-      this.id = data['id'];
-      this.edicion = data['id']  > 0;
-      this.init()
-    });
 
+  ngOnInit(): void {
     this.form = this.formbuilder.group({
-      hidCiudad: [''],
       hnombreCiudad: ['', Validators.required],
       hpoblacionCiudad: ['', Validators.required],
       hsuperficieCiudad: ['', Validators.required],
       hlatitudCiudad: ['', Validators.required],
       hlongitudCiudad: ['', Validators.required],
       hcodigoPostalCiudad: ['', Validators.required],
-      hidDepartamento: ['', Validators.required],
+      hDepartamento: ['', Validators.required],
+    });
+    this.Dep.list().subscribe((data) => {
+      this.listarDepartamento = data;
     });
   }
-
   insertar(): void {
     if (this.form.valid) {
-      this.ciudad.idCiudad = this.form.value.hidCiudad;
       this.ciudad.nombreCiudad= this.form.value.hnombreCiudad;
       this.ciudad.poblacionCiudad=this.form.value.hpoblacionCiudad;
       this.ciudad.superficieCiudad=this.form.value.hsuperficieCiudad;
       this.ciudad.latitudCiudad=this.form.value.hlatitudCiudad;
       this.ciudad.longitudCiudad=this.form.value.hlongitudCiudad;
       this.ciudad.codigoPostalCiudad=this.form.value.hcodigoPostalCiudad;
-      this.ciudad.idDepartamento=this.form.value.hidDepartamento;
-    
-      if (this.edicion) {
-        this.cS.update(this.ciudad).subscribe((data)=>{
-          this.cS.list().subscribe((data)=>{
-            this.cS.setList(data);
-          });
-        });
-      } else {
-        this.cS.insert(this.ciudad).subscribe((data) => {
-          this.cS.list().subscribe((data) => {
-            this.cS.setList(data);
-          });
-        });
-      }
-      this.router.navigate(['ciudad']);
-    }
-  }
-  init() {
-    if (this.edicion) {
-      this.cS.listId(this.id).subscribe((data) => {
-        this.form = new FormGroup({
-          hidCiudad: new FormControl(data.idCiudad),
-          hnombreCiudad: new FormControl(data.nombreCiudad),
-          hpoblacionCiudad: new FormControl(data.poblacionCiudad),
-          hsuperficieCiudad: new FormControl(data.superficieCiudad),
-          hlatitudCiudad: new FormControl(data.latitudCiudad),
-          hlongitudCiudad: new FormControl(data.longitudCiudad),
-          hcodigoPostalCiudad: new FormControl(data.codigoPostalCiudad),
-          hidDepartamento: new FormControl(data.idDepartamento)
+      this.ciudad.idDepartamento.idDepartamento=this.form.value.hDepartamento;
+      
+      this.ciuS.insert(this.ciudad).subscribe((data) => {
+        this.ciuS.list().subscribe((data) => {
+          this.ciuS.setList(data);
         });
       });
+      this.router.navigate(['ciudad']);
     }
   }
 }
