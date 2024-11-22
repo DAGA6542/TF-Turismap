b803fac06ab4759bfe5aeda43248bdc076e04dae
Joseph <u202022089@upc.edu.pe>
2024-11-21 23:17:10 -0500

    Corrigiendo frontend google api

--- Turismap/src/app/app.component.ts
+++ Turismap/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-<<<<<<< .merge_file_pK0wHD
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { PromocionComponent } from './components/promocion/promocion.component';
 import { PaisComponent } from './components/pais/pais.component';
@@ -17,28 +16,20 @@ import { LugarturisticoComponent } from './components/lugarturistico/lugarturist
 import { MenbresiavipComponent } from './components/menbresiavip/menbresiavip.component';
 import { NegocioComponent } from './components/negocio/negocio.component';
 import { RolComponent } from './components/rol/rol.component';
-=======
->>>>>>> .merge_file_ObWlTX
 import { LoginService } from './services/login.service';
 import { CommonModule } from '@angular/common';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { Router } from '@angular/router';
-
-
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< .merge_file_pK0wHD
   imports: [RouterOutlet, UsuarioComponent, PromocionComponent, PaisComponent,
     CiudadComponent, ComentarioComponent, DepartamentoComponent, FavoritoComponent, FestividadComponent,
     LugarturisticoComponent, MenbresiavipComponent, NegocioComponent, MatToolbarModule, RouterLink,
-    RolComponent, MatMenuModule, MatIconModule, MatButtonModule, CommonModule],
-=======
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule,
+    RolComponent, MatMenuModule, MatIconModule, MatButtonModule, CommonModule, RouterOutlet, RouterLink, MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule,
     CommonModule, MatListModule, MatSidenavModule, MatExpansionModule],
->>>>>>> .merge_file_ObWlTX
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
